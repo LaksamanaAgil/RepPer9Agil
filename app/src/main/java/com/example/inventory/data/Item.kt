7f5f1aa6805f53ff7f7297f7e6da631945f5f9d1
Laksamana Agil <agiladityaa@gmail.com>
2022-11-18 21:36:05 +0700

    Initial commit

---
+++ app/src/main/java/com/example/inventory/data/Item.kt
@@ -0,0 +1,34 @@
+/*
+ * Copyright (C) 2021 The Android Open Source Project.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.inventory.data
+//TODO: DIGUNAKAN UNTUK ANOTASI
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+//TODO: 'DATA' SEBELUM CLASS DIGUNAKAN UNTUK KONVERSI NAMA CLASS DARI ITEM MENJADI DATA
+@Entity
+data class Item(
+    //TODO: DEKLARASI PROPERTI DAN JENIS-JENIS MASING-MASING
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    @ColumnInfo(name = "name")
+    val itemName: String,
+    @ColumnInfo(name = "price")
+    val itemPrice: Double,
+    @ColumnInfo(name = "quantity")
+    val quantityInStock: Int,
+)
